3271f594379d816c3d56b5f3ffd06ae3774e3144
Oriana Abreu <oriabreu.v@gmail.com>
2021-04-08 00:04:31 +0200

    insert map box map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import Map from './components/Map/Map';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Map />
     </div>
   );
 }
 
 export default App;
+
