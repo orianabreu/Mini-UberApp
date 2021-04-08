26acfc6d866afa4839e354ed9f6f0f62e67ba3d0
Oriana Abreu <oriabreu.v@gmail.com>
2021-04-08 13:09:05 +0200

    change map style
    install styled-components

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -6,16 +6,17 @@ export default function Map() {
   const [viewport, setViewport] = useState({
     width: 800,
     height: 400,
-    latitude: 37.7577,
-    longitude: -122.4376,
-    zoom: 8
+    latitude: 40.420261522589406,
+    longitude: -3.6882649339095197,
+    zoom: 12
   });
-
+  
   return (
     <ReactMapGL
       {...viewport}
       onViewportChange={nextViewport => setViewport(nextViewport)}
       mapboxApiAccessToken={"pk.eyJ1Ijoib3JpYWJyZXUiLCJhIjoiY2tuN3lwbHZlMHNzNTJubng4dGxpZGw0ZSJ9.8qa_Q00dk7-BeuOoJ6yBjQ"}
+      mapStyle={"mapbox://styles/oriabreu/ckn8ronv01qc417pfz06rceka"}
     />
   );
 }
