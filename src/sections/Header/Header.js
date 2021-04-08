d4faf555e62890bd62a6d73b8f705ec25191148b
Oriana Abreu <oriabreu.v@gmail.com>
2021-04-08 14:25:29 +0200

    add header
    change map size and body background color

---
+++ src/sections/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as S from './styles';
+
+export default function Header() {
+    return (
+        <S.Title>
+            Route Cost Calculator
+        </S.Title>
+    );
+}
---
+++ src/sections/Header/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+    font-family: 'Roboto', sans-serif;
+    font-weight: 700;
+    font-size: 150px;
+`;
