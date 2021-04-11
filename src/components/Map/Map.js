2db853772c182bdb67bfcccbe66d1368e8551594
Oriana Abreu <oriabreu.v@gmail.com>
2021-04-11 19:26:18 +0200

    replace map for mapbox-gl
    update dependencies

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,34 +1,36 @@
-import * as React from 'react';
-import { useState } from 'react';
-import ReactMapGL, {GeolocateControl} from 'react-map-gl';
+import React, { useEffect, useRef } from 'react';
+import './map.css';
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions'
+import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css'
+
+mapboxgl.accessToken = "pk.eyJ1Ijoib3JpYWJyZXUiLCJhIjoiY2tuN3lwbHZlMHNzNTJubng4dGxpZGw0ZSJ9.8qa_Q00dk7-BeuOoJ6yBjQ";
 
 export default function Map() {
-  const [viewport, setViewport] = useState({
-    latitude: 40.420261522589406,
-    longitude: -3.6882649339095197,
-    zoom: 12
-  });
 
-  const geolocateControlStyle= {
-    right: 10,
-    top: 10
-  };
+  const mapContainer = useRef();
+
+    useEffect(() => {
+      const map = new mapboxgl.Map({
+        container: mapContainer.current,
+        style: 'mapbox://styles/oriabreu/ckn8ronv01qc417pfz06rceka',
+        center: [-3.68, 40.42],
+        zoom: 12
+      });
   
-  return (
-    <ReactMapGL
-      {...viewport}
-      width='100vw'
-      height='100vh'
-      onViewportChange={nextViewport => setViewport(nextViewport)}
-      mapboxApiAccessToken={"pk.eyJ1Ijoib3JpYWJyZXUiLCJhIjoiY2tuN3lwbHZlMHNzNTJubng4dGxpZGw0ZSJ9.8qa_Q00dk7-BeuOoJ6yBjQ"}
-      mapStyle={"mapbox://styles/oriabreu/ckn8ronv01qc417pfz06rceka"}
-    >
-      <GeolocateControl
-        style={geolocateControlStyle}
-        positionOptions={{enableHighAccuracy: true}}
-        trackUserLocation={true}
-        auto
-      />
-    </ReactMapGL>
-  );
+      const directions = new MapboxDirections({
+        accessToken: mapboxgl.accessToken,
+        unit: 'metric',
+        profile: 'mapbox/driving',
+      });
+  
+      map.addControl(directions, 'top-left');
+    });
+
+    return (
+      <div ref={mapContainer} className="mapWrapper" />
+    );
 }
+
+
